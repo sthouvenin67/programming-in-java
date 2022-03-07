e00e253704c89c262f6fc8fdd9a251605d1a886c
sthouvenin67 <sebastien.thouvenin@efrei.net>
2022-03-07 23:17:21 +0100

    proj1

---
+++ proj1/proj1/src/main/java/agh/ii/prinjava/proj1/impl/DLinkList.java
@@ -0,0 +1,24 @@
+package agh.ii.prinjava.proj1.impl;
+
+public class DLinkList<E> {
+    // ...
+
+    Node<T> head, tail = null;
+
+    public void addFirst(E x){
+
+    }
+
+    public void addLast(E x){
+
+    }
+
+    public remove
+
+    private static class Node<T> {
+        T elem;
+        Node<T> next;
+        Node<T> prev;
+
+    }
+}
